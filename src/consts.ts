bf143349e76c378cbf9b91dc72c2e26178a79fec
aroogit commit -m First <arek.dec@gmail.com>
2023-04-08 11:30:26 +0200

    added writings/projects table

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'evolved-eclipse';
+export const SITE_TITLE = 'Minas Mongrel';
 export const SITE_DESCRIPTION = 'Welcome to my island!';
