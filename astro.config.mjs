24d5e7c4566d9e368ade21d78a7c20ce1e3df57d
aroogit commit -m First <arek.dec@gmail.com>
2023-06-18 19:46:07 +0200

    added styling for markdown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
+import remarkGfm from 'remark-gfm';
+import a11yEmoji from '@fec/remark-a11y-emoji';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
@@ -9,5 +10,8 @@ export default defineConfig({
 	integrations: [mdx(), sitemap()],
 	experimental: {
 		assets: true
-	   }
+	   },
+	markdown: {
+		RemarkPlugins: [remarkGfm, a11yEmoji]
+	}
 });
