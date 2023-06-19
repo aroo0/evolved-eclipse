99faafc99f21877f8009a3d1b720383f4b836aaf
aroogit commit -m First <arek.dec@gmail.com>
2023-06-19 18:12:23 +0200

    finished post styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,18 @@ import mdx from '@astrojs/mdx';
 import remarkGfm from 'remark-gfm';
 import a11yEmoji from '@fec/remark-a11y-emoji';
 import sitemap from '@astrojs/sitemap';
+import markdownItSup from 'markdown-it-sup';
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
-	experimental: {
-		assets: true
-	   },
-	markdown: {
-		RemarkPlugins: [remarkGfm, a11yEmoji]
-	}
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), react()],
+  experimental: {
+    assets: true
+  },
+  markdown: {
+    RemarkPlugins: [remarkGfm, a11yEmoji]
+  }
 });
--- src/content/config.ts
+++ src/content/config.ts
@@ -4,6 +4,7 @@ const blog = defineCollection({
 	// Type-check frontmatter using a schema
 	schema: z.object({
 		title: z.string(),
+		isDraft: z.boolean(),
 		description: z.string(),
 		// Transform string to Date object
 		pubDate: z
