e8fd001739089dde7bb7fb891ce0adaf3334c872
aroogit commit -m First <arek.dec@gmail.com>
2023-06-20 14:06:10 +0200

    building table of posts

--- src/components/PostTable.jsx
+++ src/components/PostTable.jsx
@@ -6,26 +6,34 @@ export default function PostTable({ posts }) {
     
     return (
       <>
-        {/* <ul>
+
+<div class='post-table'>
+
+<div class="row row-headline">
+    <span class='headline row-title '>Title</span>
+    <span class='headline row-tags '>Tags</span>
+    <span class='headline row-date '>Date</span>
+    <hr />
+</div>
+        <ul class='table'>
           {posts.map((post) => (
-            <li key={post.slug}>
-              <time dateTime={post.data.pubDate.toISOString()}>
-                {formattedDate(post.data.pubDate)}
-                </time>
-              <a href={`/blog/${post.slug}/`}>{post.data.title}</a>
+            <li key={post.slug} class='row'>
+                <span class='row-title cell-title'>
+                  <a href={`/blog/${post.slug}/`}>{post.data.title}</a>
+                </span>
+                <ul class='row-tags cell-tags'>{post.data.tags.map(tag => (
+                    <li class='tag-pseudo-link'>{tag}</li>
+                ))}</ul>
+
+                <span class='row-date cell-date'>
+                  <time dateTime={post.data.pubDate.toISOString()}>
+                  {formattedDate(post.data.pubDate)}
+                  </time>
+                </span>
             </li>
           ))}
-        </ul> */}
-
-<div class='post-table table__main'>
-
-<div class="row row-">
-    <span class='table__headline row__title cell'>Title</span>
-    <span class='table__headline row__publisher cell'>Tags</span>
-    <span class='table__headline row__date cell'>Date</span>
-</div>
-
-
+        </ul>
+        
 </div>
       </>
     );
