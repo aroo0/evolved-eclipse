7719215f69c1b1ff51e01058aa93b2e09e4a2fa2
aroogit commit -m First <arek.dec@gmail.com>
2023-06-22 17:15:29 +0200

    test

--- src/components/PostTable.jsx
+++ src/components/PostTable.jsx
@@ -49,7 +49,7 @@ export default function PostTable({ posts }) {
 
   return (
     <>
-    <div class="content-wrapper">
+    <div class="content-wrapper" data-pagefind-ignore>
     <div className="selected-tags-container tags">
         {selectedTags.map((tag) => (
           <span key={tag} className="selected-tag tag black-filled" onClick={() => handleTagRemove(tag)}>
