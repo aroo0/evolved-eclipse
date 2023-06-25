aee6be0a1d88b35d7d2fb73f44d42af740183d53
aroogit commit -m First <arek.dec@gmail.com>
2023-06-25 21:35:32 +0200

    finished writing page

--- src/components/ProjectsTable.tsx
+++ src/components/ProjectsTable.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import projectsData from '../pages/projects-writings/data.json';
+import ArowIcon from './ArowIcon'
 
 interface Project {
   id: number;
@@ -11,9 +12,16 @@ interface Project {
   lang: string;
 }
 
+type Language = "ALL" | "pl" | "en";
+
+
 export default function ProjectsTable() {
   const projects: Project[] = projectsData;
-  const [selectedLanguage, setSelectedLanguage] = useState("ALL");
+  const [selectedLanguage, setSelectedLanguage] = useState<Language>("ALL");
+  const [sortingOrder, setSortingOrder] = useState<{ column: string; order: "asc" | "desc" }>({
+    column: "date",
+    order: "desc",
+  });
 
   // Filter projects based on the selected language
   const filteredProjects = projects.filter((project) => {
@@ -24,16 +32,48 @@ export default function ProjectsTable() {
     }
   });
 
+  // Sort projects based on the selected sorting order and criteria
+  const sortedProjects = filteredProjects.sort((a, b) => {
+    const column = sortingOrder.column;
+    const order = sortingOrder.order;
+
+    if (column === "title") {
+      return order === "asc" ? a.title.localeCompare(b.title) : b.title.localeCompare(a.title);
+    } else if (column === "publisher") {
+      return order === "asc" ? a.publisher.localeCompare(b.publisher) : b.publisher.localeCompare(a.publisher);
+    } else if (column === "media") {
+      return order === "asc" ? a.type.localeCompare(b.type) : b.type.localeCompare(a.type);
+    } else if (column === "date") {
+      return order === "asc" ? a.date.localeCompare(b.date) : b.date.localeCompare(a.date);
+    }
+
+    return 0;
+  });
+
   // Function to handle filter change
   const handleFilterChange = (lang: string) => {
     setSelectedLanguage(lang);
+  };
+
+  // Function to handle sorting change
+  const handleSortingChange = (column: string) => {
+    setSortingOrder((prevSortingOrder) => {
+      if (prevSortingOrder.column === column) {
+        // If the same column is clicked, toggle the sorting order
+        const newOrder = prevSortingOrder.order === "asc" ? "desc" : "asc";
+        return { column, order: newOrder };
+      } else {
+        // If a different column is clicked, set the new column and default to ascending order
+        return { column, order: "asc" };
+      }
+    });
   };
 
   return (
     <>
       <div className='project-table' data-pagefind-ignore>
         <div className='table__lang'>
-          <div className='headline headline-border'>Lang</div>
+          <div className='headline headline'>Lang</div>
           <hr />
           <div className="lang-list">
             <span
@@ -58,14 +98,38 @@ export default function ProjectsTable() {
         </div>
         <div className='table-main'>
           <div className="row row-headline">
-            <span className='headline projects-row-title headline-border'>Title</span>
-            <span className='headline projects-row-publisher headline-border'>Publisher</span>
-            <span className='headline projects-row-media headline-border'>Media</span>
-            <span className='headline projects-row-date headline-border'>Date</span>
+            <div
+              className='headline projects-row-title headline-border'
+              onClick={() => handleSortingChange("title")}
+            >
+              Title
+              <ArowIcon />
+            </div>
+            <div
+              className='headline projects-row-publisher headline-border'
+              onClick={() => handleSortingChange("publisher")}
+            >
+              Publisher
+              <ArowIcon />
+            </div>
+            <div
+              className='headline projects-row-media headline-border'
+              onClick={() => handleSortingChange("media")}
+            >
+              Media
+              <ArowIcon />
+            </div>
+            <div
+              className='headline projects-row-date headline-border'
+              onClick={() => handleSortingChange("date")}
+            >
+              Date
+              <ArowIcon />
+            </div>
             <hr className="project-writings-hr" />
           </div>
           <ul className="table">
-            {filteredProjects.map((project) => (
+            {sortedProjects.map((project) => (
               <li className="row" key={project.id}>
                 <span className='projects-row-title project-cell-title'>
                   {project.url ? (
