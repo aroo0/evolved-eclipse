254796515940e9f1f817c2166ed08a5286e2300c
aroogit commit -m First <arek.dec@gmail.com>
2023-07-10 18:23:13 +0200

    changed page desc

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Minas Mongrel';
-export const SITE_DESCRIPTION = 'Welcome to my island!';
+export const SITE_DESCRIPTION = '';
