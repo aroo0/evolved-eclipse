89e8e66b1c238ccc0724b7763080d9b40cd40933
aroogit commit -m First <arek.dec@gmail.com>
2023-08-12 13:20:34 +0200

    try to delete netlfiy folder

---
+++ .vercel/output/static/pagefind/pagefind.js
@@ -0,0 +1,1 @@
+export const search = () => {return {results: []}}
--- astro.config.mjs
+++ astro.config.mjs
@@ -3,19 +3,20 @@ import mdx from '@astrojs/mdx';
 import remarkGfm from 'remark-gfm';
 import sitemap from '@astrojs/sitemap';
 import react from "@astrojs/react";
+import node from "@astrojs/node";
 
-import netlify from "@astrojs/netlify/functions";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://minasmongrel.xyz/',
-  adapter: netlify(),
+  output: 'hybrid',
+  adapter: vercel(),
   integrations: [mdx(), sitemap(), react()],
   experimental: {
     assets: true
   },
   markdown: {
     RemarkPlugins: [remarkGfm]
-  },
-  output: "server"
+  }
 });
